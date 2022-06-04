07dba5503eca4c119a6bd93395ff96122ebd539c
Carly Richardson <cbrich16@gmail.com>
2022-06-04 14:27:17 -0500

    Added overlay to earthquakes map.

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -24,6 +24,15 @@ let baseMaps = {
   "Satellite": satelliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.LayerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [39.5, -98.15],
@@ -32,7 +41,7 @@ let map = L.map('mapid', {
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL
 //let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
@@ -94,16 +103,18 @@ function getRadius(magnitude) {
 // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
 // We turn each feature into a circleMarker on the map.
-pointToLayer: function(feature, latlng) {
-    console.log(data);
-    return L.circleMarker(latlng);
-  },
-style: styleInfo,
+      pointToLayer: function(feature, latlng) {
+          console.log(data);
+          return L.circleMarker(latlng);
+        },
+    style: styleInfo,
     // We create a popup for each circleMarker to display the magnitude and
     //  location of the earthquake after the marker has been created and styled.
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-    }
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }    
+  }).addTo(earthquakes);
 
-}).addTo(map);
+    // Then we add the earthquake layer to our map.
+    earthquakes.addTo(map);
 });
